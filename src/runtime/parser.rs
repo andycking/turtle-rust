44e47c59f94911874705e6a48fe46450117112ce
Andy C King <andycking@fastmail.com>
2021-05-25 10:23:03 -0500

    Just keep entering garbage

--- src/runtime/parser.rs
+++ src/runtime/parser.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::any::Any;
+use std::fmt;
 use std::ops::Deref;
 use std::ops::DerefMut;
 
@@ -21,17 +23,40 @@ use super::error::InterpreterError::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum InstructionTag {
+    Call,
     Home,
-    Make,
+    MakeVar,
+    MakeProc,
     Move,
     Pen,
-    Procedure,
     Rotate,
     SetPosition,
 }
 
 trait Instruction {
     fn tag(&self) -> InstructionTag;
+    fn as_any(&self) -> &dyn Any;
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct CallInstruction {
+    name: Word,
+}
+
+impl Instruction for CallInstruction {
+    fn tag(&self) -> InstructionTag {
+        InstructionTag::Call
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl CallInstruction {
+    pub fn new(name: Word) -> Self {
+        Self { name }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -41,6 +66,10 @@ impl Instruction for HomeInstruction {
     fn tag(&self) -> InstructionTag {
         InstructionTag::Home
     }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl HomeInstruction {
@@ -49,19 +78,67 @@ impl HomeInstruction {
     }
 }
 
+type InstructionListItems = Vec<Box<dyn Instruction>>;
+
+struct MakeProcInstruction {
+    name: Word,
+    instrs: InstructionListItems,
+}
+
+impl Instruction for MakeProcInstruction {
+    fn tag(&self) -> InstructionTag {
+        InstructionTag::MakeProc
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Deref for MakeProcInstruction {
+    type Target = InstructionListItems;
+
+    fn deref(&self) -> &Self::Target {
+        &self.instrs
+    }
+}
+
+impl DerefMut for MakeProcInstruction {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.instrs
+    }
+}
+
+impl MakeProcInstruction {
+    pub fn new(name: Word) -> Self {
+        Self {
+            name,
+            instrs: Vec::new(),
+        }
+    }
+
+    pub fn consume(&mut self) -> InstructionListItems {
+        std::mem::replace(&mut self.instrs, Vec::new())
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
-struct MakeInstruction {
+struct MakeVarInstruction {
     name: Word,
     value: Word,
 }
 
-impl Instruction for MakeInstruction {
+impl Instruction for MakeVarInstruction {
     fn tag(&self) -> InstructionTag {
-        InstructionTag::Make
+        InstructionTag::MakeVar
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
-impl MakeInstruction {
+impl MakeVarInstruction {
     pub fn new(name: Word, value: Word) -> Self {
         Self { name, value }
     }
@@ -82,6 +159,10 @@ struct MoveInstruction {
 impl Instruction for MoveInstruction {
     fn tag(&self) -> InstructionTag {
         InstructionTag::Move
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -111,28 +192,15 @@ impl Instruction for PenInstruction {
     fn tag(&self) -> InstructionTag {
         InstructionTag::Pen
     }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl PenInstruction {
     pub fn new(op: PenOperation, color: Option<Word>) -> Self {
         Self { op, color }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct ProcInstruction {
-    name: Word,
-}
-
-impl Instruction for ProcInstruction {
-    fn tag(&self) -> InstructionTag {
-        InstructionTag::Pen
-    }
-}
-
-impl ProcInstruction {
-    pub fn new(name: Word) -> Self {
-        Self { name }
     }
 }
 
@@ -152,6 +220,10 @@ impl Instruction for RotateInstruction {
     fn tag(&self) -> InstructionTag {
         InstructionTag::Rotate
     }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl RotateInstruction {
@@ -170,6 +242,10 @@ impl Instruction for SetPositionInstruction {
     fn tag(&self) -> InstructionTag {
         InstructionTag::SetPosition
     }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl SetPositionInstruction {
@@ -178,23 +254,33 @@ impl SetPositionInstruction {
     }
 }
 
-pub type InstructionListItems = Vec<Box<dyn Instruction>>;
+impl fmt::Debug for dyn Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.tag() {
+            InstructionTag::Call => {
+                let instr = self.as_any().downcast_ref::<CallInstruction>().unwrap();
+                write!(f, "call {:?}", instr.name)
+            }
 
-struct InstructionList {
-    items: InstructionListItems,
-}
+            InstructionTag::Home => {
+                let instr = self.as_any().downcast_ref::<HomeInstruction>().unwrap();
+                write!(f, "home")
+            }
 
-impl Deref for InstructionList {
-    type Target = InstructionListItems;
+            InstructionTag::Move => {
+                let instr = self.as_any().downcast_ref::<MoveInstruction>().unwrap();
+                write!(f, "move {:?} {:?}", instr.direction, instr.distance)
+            }
 
-    fn deref(&self) -> &Self::Target {
-        &self.items
-    }
-}
+            InstructionTag::Pen => {
+                let instr = self.as_any().downcast_ref::<PenInstruction>().unwrap();
+                write!(f, "pen {:?}", instr.op)
+            }
 
-impl DerefMut for InstructionList {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.items
+            _ => {
+                write!(f, "Unkown")
+            }
+        }
     }
 }
 
@@ -279,12 +365,14 @@ impl<'a> ListIter<'a> {
 
 pub struct Parser<'a> {
     iter: ListIter<'a>,
+    instrs: MakeProcInstruction,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(input: &'a List) -> Self {
         Self {
             iter: ListIter::new(input),
+            instrs: MakeProcInstruction::new(Word::new("main", WordAttr::Bare)),
         }
     }
 
@@ -371,6 +459,11 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let distance = self.iter.get_argument()?.clone();
         let instr = MoveInstruction::new(distance, MoveDirection::Backwards);
+        self.instrs.push(Box::new(instr));
+        Ok(())
+    }
+
+    fn end(&mut self) -> Result<(), InterpreterError> {
         Ok(())
     }
 
@@ -378,27 +471,13 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let distance = self.iter.get_argument()?.clone();
         let instr = MoveInstruction::new(distance, MoveDirection::Forwards);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
     fn home(&mut self) -> Result<(), InterpreterError> {
         let instr = HomeInstruction::new();
-        Ok(())
-    }
-
-    fn make(&mut self) -> Result<(), InterpreterError> {
-        self.iter.expect(2)?;
-        let name = self.iter.get_quoted()?.clone();
-        let value = self.iter.get_argument()?.clone();
-        let instr = MakeInstruction::new(name, value);
-        Ok(())
-    }
-
-    fn name(&mut self) -> Result<(), InterpreterError> {
-        self.iter.expect(2);
-        let value = self.iter.get_argument()?.clone();
-        let name = self.iter.get_quoted()?.clone();
-        let instr = MakeInstruction::new(name, value);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -406,16 +485,37 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let angle = self.iter.get_argument()?.clone();
         let instr = RotateInstruction::new(angle, RotateDirection::Left);
+        self.instrs.push(Box::new(instr));
+        Ok(())
+    }
+
+    fn make(&mut self) -> Result<(), InterpreterError> {
+        self.iter.expect(2)?;
+        let name = self.iter.get_quoted()?.clone();
+        let value = self.iter.get_argument()?.clone();
+        let instr = MakeVarInstruction::new(name, value);
+        self.instrs.push(Box::new(instr));
+        Ok(())
+    }
+
+    fn name(&mut self) -> Result<(), InterpreterError> {
+        self.iter.expect(2)?;
+        let value = self.iter.get_argument()?.clone();
+        let name = self.iter.get_quoted()?.clone();
+        let instr = MakeVarInstruction::new(name, value);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
     fn pendown(&mut self) -> Result<(), InterpreterError> {
         let instr = PenInstruction::new(PenOperation::Down, None);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
     fn penup(&mut self) -> Result<(), InterpreterError> {
         let instr = PenInstruction::new(PenOperation::Up, None);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -423,6 +523,7 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let angle = self.iter.get_argument()?.clone();
         let instr = RotateInstruction::new(angle, RotateDirection::Right);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -430,6 +531,7 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let color = self.iter.get_argument()?;
         let instr = PenInstruction::new(PenOperation::SetColor, Some(color.clone()));
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -441,6 +543,7 @@ impl<'a> Parser<'a> {
         let x = pos_iter.get_argument()?.clone();
         let y = pos_iter.get_argument()?.clone();
         let instr = SetPositionInstruction::new(Some(x), Some(y));
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -449,6 +552,7 @@ impl<'a> Parser<'a> {
         let x = self.iter.get_argument()?.clone();
         let y = self.iter.get_argument()?.clone();
         let instr = SetPositionInstruction::new(Some(x), Some(y));
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -456,6 +560,7 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let x = self.iter.get_argument()?.clone();
         let instr = SetPositionInstruction::new(Some(x), None);
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
@@ -463,17 +568,15 @@ impl<'a> Parser<'a> {
         self.iter.expect(1)?;
         let y = self.iter.get_argument()?.clone();
         let instr = SetPositionInstruction::new(None, Some(y));
-        Ok(())
-    }
-
-    fn end(&mut self) -> Result<(), InterpreterError> {
+        self.instrs.push(Box::new(instr));
         Ok(())
     }
 
     fn to(&mut self) -> Result<(), InterpreterError> {
-        self.iter.expect(2)?;
+        self.iter.expect(1)?;
         let name = self.iter.get_argument()?.clone();
-        let instr = ProcInstruction::new(name);
+        let instr = MakeProcInstruction::new(name);
+        // Hrm.
         Ok(())
     }
 }
