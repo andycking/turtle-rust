51fdd0db67b34c8269941e3ce7678a34d38a349d
Andy C King <andycking@fastmail.com>
2021-05-25 13:23:14 -0500

    Format

--- src/runtime/instr.rs
+++ src/runtime/instr.rs
@@ -19,7 +19,7 @@ use super::data_type::*;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct CallInstruction {
-	name: Word,
+    name: Word,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -27,123 +27,123 @@ pub struct HomeInstruction {}
 
 #[derive(Clone, Debug)]
 pub struct MakeProcInstruction {
-	name: Word,
-	list: Vec<Instruction>,
+    name: Word,
+    list: Vec<Instruction>,
 }
 
 impl MakeProcInstruction {
-	pub fn new(name: Word) -> Self {
-		Self {
-			name,
-			list: Vec::new(),
-		}
-	}
+    pub fn new(name: Word) -> Self {
+        Self {
+            name,
+            list: Vec::new(),
+        }
+    }
 }
 
 impl Deref for MakeProcInstruction {
-	type Target = Vec<Instruction>;
+    type Target = Vec<Instruction>;
 
-	fn deref(&self) -> &Self::Target {
-		&self.list
-	}
+    fn deref(&self) -> &Self::Target {
+        &self.list
+    }
 }
 
 impl DerefMut for MakeProcInstruction {
-	fn deref_mut(&mut self) -> &mut Self::Target {
-		&mut self.list
-	}
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.list
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct MakeVarInstruction {
-	name: Word,
-	value: Word,
+    name: Word,
+    value: Word,
 }
 
 impl MakeVarInstruction {
-	pub fn new(name: Word, value: Word) -> Self {
-		Self { name, value }
-	}
+    pub fn new(name: Word, value: Word) -> Self {
+        Self { name, value }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum MoveDirection {
-	Forwards,
-	Backwards,
+    Forwards,
+    Backwards,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct MoveInstruction {
-	distance: Word,
-	direction: MoveDirection,
+    distance: Word,
+    direction: MoveDirection,
 }
 
 impl MoveInstruction {
-	pub fn new(distance: Word, direction: MoveDirection) -> Self {
-		Self {
-			distance,
-			direction,
-		}
-	}
+    pub fn new(distance: Word, direction: MoveDirection) -> Self {
+        Self {
+            distance,
+            direction,
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum PenOperation {
-	Down,
-	Up,
-	SetColor,
+    Down,
+    Up,
+    SetColor,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct PenInstruction {
-	op: PenOperation,
-	color: Option<Word>,
+    op: PenOperation,
+    color: Option<Word>,
 }
 
 impl PenInstruction {
-	pub fn new(op: PenOperation, color: Option<Word>) -> Self {
-		Self { op, color }
-	}
+    pub fn new(op: PenOperation, color: Option<Word>) -> Self {
+        Self { op, color }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum RotateDirection {
-	Left,
-	Right,
+    Left,
+    Right,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct RotateInstruction {
-	angle: Word,
-	direction: RotateDirection,
+    angle: Word,
+    direction: RotateDirection,
 }
 
 impl RotateInstruction {
-	pub fn new(angle: Word, direction: RotateDirection) -> Self {
-		Self { angle, direction }
-	}
+    pub fn new(angle: Word, direction: RotateDirection) -> Self {
+        Self { angle, direction }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct SetPositionInstruction {
-	x: Option<Word>,
-	y: Option<Word>,
+    x: Option<Word>,
+    y: Option<Word>,
 }
 
 impl SetPositionInstruction {
-	pub fn new(x: Option<Word>, y: Option<Word>) -> Self {
-		Self { x, y }
-	}
+    pub fn new(x: Option<Word>, y: Option<Word>) -> Self {
+        Self { x, y }
+    }
 }
 
 #[derive(Clone, Debug)]
 pub enum Instruction {
-	Call(CallInstruction),
-	Home(HomeInstruction),
-	MakeProc(MakeProcInstruction),
-	MakeVar(MakeVarInstruction),
-	Move(MoveInstruction),
-	Pen(PenInstruction),
-	Rotate(RotateInstruction),
-	SetPosition(SetPositionInstruction),
+    Call(CallInstruction),
+    Home(HomeInstruction),
+    MakeProc(MakeProcInstruction),
+    MakeVar(MakeVarInstruction),
+    Move(MoveInstruction),
+    Pen(PenInstruction),
+    Rotate(RotateInstruction),
+    SetPosition(SetPositionInstruction),
 }
