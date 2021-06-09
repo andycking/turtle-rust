3a065e7609a862ef0d9d3f5461f06d7069a7c371
Andy C King <andycking@fastmail.com>
2021-06-09 09:17:04 -0500

    Oh good

---
+++ src/common/bits.rs
@@ -0,0 +1,24 @@
+// Copyright 2021 Andy King
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::ptr;
+use std::sync::atomic;
+
+#[inline]
+pub fn zero<T>(input: &mut [T]) {
+    atomic::fence(atomic::Ordering::SeqCst);
+    unsafe {
+        ptr::write_bytes(input.as_mut_ptr(), 0, input.len());
+    }
+}
