db0df1db5abead86c9fee2ddc70899eeab10694a
Andy C King <andycking@fastmail.com>
2021-06-15 20:33:22 -0500

    Example code

--- src/controller/examples.rs
+++ src/controller/examples.rs
@@ -25,6 +25,7 @@ pub fn show(_ctx: &mut DelegateCtx, cmd: &druid::Command, data: &mut AppState) {
         "color-star" => include_str!("../assets/color-star.logo"),
         "fan-flower" => include_str!("../assets/fan-flower.logo"),
         "fill" => include_str!("../assets/fill.logo"),
+        "for-loop" => include_str!("../assets/for-loop.logo"),
         "spin-wheel" => include_str!("../assets/spin-wheel.logo"),
         "spiral" => include_str!("../assets/spiral.logo"),
         "squares" => include_str!("../assets/squares.logo"),
